6ff5a88251f52f4d7bc3abc9f0079bbe2862e7cc
Ipek Turgul <ipek.turgul@immutable.com>
2023-06-26 13:35:23 +1000

    add transfer

--- scripts/transferNFT.ts
+++ scripts/transferNFT.ts
@@ -1,6 +1,7 @@
 import { ethers } from 'hardhat';
 import { MyERC721__factory } from '../typechain-types';
 
+// Set your own contract here
 const CONTRACT_ADDRESS = '0xAC67fda08d891728f6449c45c9AF45A283fAFA31';
 
 async function deploy() {
@@ -19,11 +20,12 @@ async function deploy() {
 
   const contract = factory.attach(CONTRACT_ADDRESS);
 
+  // Run this first - running both at the same returns error
   // grant minter role to owner
   // await contract.connect(owner).grantMinterRole(owner.address);
 
   // mint
-  const mintResult = await contract.connect(owner).mint(owner.address, 1);
+  const mintResult = await contract.connect(owner)["safeTransferFrom(address,address,uint256)"](owner.address,"0x0000000000000000000000000000000000000000", 2)
   console.log(`MyERC721 mint transaction hash: ${mintResult.hash}`);
 }
 
