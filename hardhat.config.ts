de7a2c6498c2146b1e6e7aa7fb8e2d08a3a9cc13
James Snewin <james.snewin@immutable.com>
2023-06-28 18:35:10 +1000

    update network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,13 +9,10 @@ dotenv.config();
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
   networks: {
-    goerli: {
-      url: process.env.GOERLI_URL || "",
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
-    },
     immutableZkevmTestnet: {
-      url: process.env.IMMUTABLE_ZKEVM_URL || "",
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      url: "http://rpc.testnet.immutable.com",
+      accounts:
+      process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     },
   },
   etherscan: {
